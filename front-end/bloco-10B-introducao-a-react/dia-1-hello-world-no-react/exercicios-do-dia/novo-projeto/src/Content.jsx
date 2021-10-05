bcc5dd3d3ce77ce03f4d727642f1744762610ee8
Maria Laura <marialaurap27@gmail.com>
2021-10-05 14:24:54 -0300

    n

---
+++ front-end/bloco-10B-introducao-a-react/dia-1-hello-world-no-react/exercicios-do-dia/novo-projeto/src/Content.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+const conteudos = [
+    {
+      conteudo: 'High Order Functions',
+      bloco: 8,
+      status: 'Aprendido'
+    },
+    {
+      conteudo: 'Composicao de Componentes',
+      bloco: 11,
+      status: 'Aprendendo',
+    },
+    {
+      conteudo: 'Composicao de Estados',
+      bloco: 12,
+      status: 'Aprenderei'
+    },
+    {
+      conteudo: 'Redux',
+      bloco: 16,
+      status: 'Aprenderei'
+    },
+  ];
+
+class Content extends React.Component {
+    render() {
+        return (
+            <div>
+                {conteudos.map((item) => 
+                    <div>
+                    <span>{`O conteúdo é ${item.conteudo}`}</span>
+                    <p>{`status: ${item.status}`}</p>
+                    <p>{`bloco: ${item.bloco}`}</p>
+                    </div>
+                    
+                )}
+            </div>
+        )
+    }
+}
+
+export default Content;
